2186c5d1614afe7e02d916977bf0d6524ff80813
djh0211 <djh0211@konkuk.ac.kr>
2024-04-23 15:15:16 +0100

    image capture code done

--- opencv_py/qr/qr_skeleton.py
+++ opencv_py/qr/qr_skeleton.py
@@ -1,22 +1,30 @@
-import cv2
+#!/usr/bin/python3
 
-# 카메라 객체 생성
-cap = cv2.VideoCapture(0)
+# Capture a full resolution image to memory rather than to a file.
 
-while True:
-    # 프레임 캡처
-    ret, frame = cap.read()
-    
-    # 프레임 처리 (예: 그레이스케일 변환)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # 처리된 프레임 출력
-    cv2.imshow('frame', gray)
-    
-    # 'q' 키를 누르면 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+import time
 
-# 자원 해제
-cap.release()
-cv2.destroyAllWindows()
+from picamera2 import Picamera2, Preview
+from libcamera import controls
+import numpy as np
+
+picam2 = Picamera2()
+picam2.start_preview(Preview.QTGL)
+preview_config = picam2.create_preview_configuration(buffer_count=3)
+capture_config = picam2.create_still_configuration(buffer_count=3)
+
+picam2.configure(preview_config)
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+picam2.start()
+time.sleep(2)
+
+picam2.switch_mode(capture_config)
+array = picam2.capture_array() 
+np.save('./qr', array)
+# image = picam2.switch_mode_and_capture_image(capture_config)
+# image.show()
+
+
+time.sleep(5)
+
+picam2.close()
